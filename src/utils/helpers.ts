dfef2243a2c2235eb06e310a8bfd5e21d6d7dff0
Nabcellent <nabcellent.dev@gmail.com>
2022-08-10 13:26:25 +0300

    Adds description to savings transaction tables
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,7 +1,5 @@
 import withReactContent from 'sweetalert2-react-content';
 import Swal, { SweetAlertOptions } from 'sweetalert2';
-import { parsePhoneNumber } from 'libphonenumber-js';
-import { Telco } from './enums';
 
 export const getItemFromStore = (key: string, defaultValue?: string | boolean, store = localStorage) => {
     try {
@@ -81,54 +79,11 @@ export const toast = async (data: SweetAlertOptions) => {
     await Sweet.fire(options);
 };
 
-export const isIterableArray = (array: any) => Array.isArray(array) && !!array.length;
-
-export const Str = {
-    headline: (str: string) => {
-        if (!str) return "";
-
-        str = str.replaceAll('_', ' ').replaceAll('-', ' ');
-
-        return str.replaceAll(/\w\S*/g, (t) => t.charAt(0).toUpperCase() + t.substring(1).toLowerCase());
-    },
-    ucFirst: (str: string) => {
-        str = str.toLowerCase();
-
-        return str.charAt(0).toUpperCase() + str.slice(1);
-    }
-};
-
 export const currencyFormat = (number?: number, currency = 'KES') => number && (new Intl.NumberFormat('en-GB', {
     style: 'currency',
     currency,
     maximumFractionDigits: 4
 })).format(number);
-
-export const parsePhone = (phone?: string | number) => phone && parsePhoneNumber(String(phone), 'KE').number;
-
-export const getTelcoFromPhone = (phone: string | number) => {
-    phone = String(phone);
-
-    const safRegEx = /^(?:254|\+254|0)?((?:7(?:[0129]\d|4[0123568]|5[789]|6[89])|(1(1[0-5])))\d{6})$/,
-        airtelRegEx = /^(?:254|\+254|0)?((?:(7(?:(3\d)|(5[0-6])|(6[27])|(8\d)))|(1(0[0-6])))\d{6})$/,
-        telkomRegEx = /^(?:254|\+254|0)?(7(7\d)\d{6})$/,
-        equitelRegEx = /^(?:254|\+254|0)?(7(6[3-6])\d{6})$/,
-        faibaRegEx = /^(?:254|\+254|0)?(747\d{6})$/;
-
-    if (phone.match(safRegEx)) {
-        return Telco.SAFARICOM;
-    } else if (phone.match(airtelRegEx)) {
-        return Telco.AIRTEL;
-    } else if (phone.match(telkomRegEx)) {
-        return Telco.TELKOM;
-    } else if (phone.match(equitelRegEx)) {
-        return Telco.EQUITEL;
-    } else if (phone.match(faibaRegEx)) {
-        return Telco.FAIBA;
-    } else {
-        return null;
-    }
-};
 
 export const colors = [
     '#2c7be5',
